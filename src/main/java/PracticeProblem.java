7c452a196b4ccc1601dbb8b6c423271bfbb1bbbc
unikornnaphat <350364092@tdsb.ca>
2026-02-28 21:44:29 +0000

    1.7 practicfe problem-sandy

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -10,27 +10,52 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		boolean bool1 = true;
+        boolean bool2 = false;
+        System.out.println(bool1 && bool2);
+        System.out.println(bool1 || bool2);
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a lowercase letter: ");
+        char c1 = scanner.next().charAt(0);
+        System.out.print("Input another lowercase letter: ");
+        char c2 = scanner.next().charAt(0);
+        System.out.println(c1 < c2);
+        scanner.close();
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        double num = scanner.nextDouble();
+        System.out.println(num >= 0 && num <= 10);
+        scanner.close();
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        double num = scanner.nextDouble();
+        System.out.println(num > 0 && num != 5);
+        scanner.close();
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a word: ");
+        String word = scanner.next();
+        System.out.println(!word.equals("banana"));
+        scanner.close();
 	}
 
 	public static void q6() {
-		//Write question 5 code here
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+        System.out.println(num % 2 == 0);
+        scanner.close();
 	}
 
 }
